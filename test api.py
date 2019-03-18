d7763c898f46943713033ff79b3719f2a4f408ef
somphon <45285645+somphonma@users.noreply.github.com>
2019-03-18 18:53:58 +0700

    Update test api.py

--- test api.py
+++ test api.py
@@ -6,6 +6,16 @@ print(couriers)
 '''
 import aftership
 api = aftership.APIv4('95ce5ddb-72f6-4e0c-9114-a185374ec85d')
-slug = 'thailand-post'
-number = 'EW332041693TH'
+couriers = api.couriers.all.get()
+print(couriers)
+#slug = 'thailand-post'
+slug = 'kerry-logistics'
+#number = 'EW332041693TH'
+number ='SMMS000109643'
+detail=api.last_checkpoint.get(slug, number, fields=['title', 'created_at','customer_name','checkpoint_time','city','country_iso3','tag'])
+print(detail)
+id=detail['id']
+tracking_number=detail['tracking_number']
 
+
+print(id)
