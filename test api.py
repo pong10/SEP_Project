54775a85b3aae7fc156048a504ff61d00c8bbf87
somphon <45285645+somphonma@users.noreply.github.com>
2019-03-18 18:55:02 +0700

    Update test api.py

--- test api.py
+++ test api.py
@@ -1,9 +1,3 @@
-'''
-import aftership
-api = aftership.APIv4('95ce5ddb-72f6-4e0c-9114-a185374ec85d')
-couriers = api.couriers.all.get()
-print(couriers)
-'''
 import aftership
 api = aftership.APIv4('95ce5ddb-72f6-4e0c-9114-a185374ec85d')
 couriers = api.couriers.all.get()
