ff211f0c7f396aaac86025ccdbb41fcdd871867d
pong10 <45727975+pong10@users.noreply.github.com>
2019-03-18 18:30:36 +0700

    Update test api.py

--- test api.py
+++ test api.py
@@ -1,4 +1,11 @@
+'''
 import aftership
-api = aftership.APIv4('270908a6-934b-45cd-bada-b47c2d82792c')
+api = aftership.APIv4('95ce5ddb-72f6-4e0c-9114-a185374ec85d')
 couriers = api.couriers.all.get()
 print(couriers)
+'''
+import aftership
+api = aftership.APIv4('95ce5ddb-72f6-4e0c-9114-a185374ec85d')
+slug = 'thailand-post'
+number = 'EW332041693TH'
+
